29bcc1910e31e2ffb90a8dafc0ba1618e4e62bde
brijesh23-dev <rbrijesh480@gmail.com>
2026-03-23 23:09:48 +0530

    accessToken refressToken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -25,11 +25,19 @@ module.exports.Register = async (req, res) => {
   });
   await user.save();
 
-  const token = jwt.sign({ id: user._id }, config.JWT_SECRET, {
-    expiresIn: "1h",
+  const accessToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+    expiresIn: "7d",
   });
 
-  res.cookie("token", token);
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
 
   res.status(201).json({
     message: "user register successfully",
@@ -38,9 +46,8 @@ module.exports.Register = async (req, res) => {
       username: user.username,
       email: user.email,
     },
-    token,
+    accessToken,
   });
-
 };
 
 module.exports.Login = async (req, res) => {
@@ -56,17 +63,16 @@ module.exports.Login = async (req, res) => {
   }
 
   const isPasswordMatch = await bcrypt.compare(password, user.password);
-  
+
   if (!isPasswordMatch) {
     res.status(401).json({
       message: "Invalid credentials",
     });
   }
-  
+
   const token = jwt.sign({ id: user._id }, config.JWT_SECRET, {
     expiresIn: "1h",
   });
-
 
   res.status(200).json({
     message: "Login successful",
@@ -79,25 +85,58 @@ module.exports.Login = async (req, res) => {
   });
 };
 
-module.exports.getme = async(req,res)=>{
-  const token  =  req.headers.authorization?.split(" ")[1];
-  
-  if(!token){
-    return res.status(401).json({message:"Unauthorized"})
+module.exports.getme = async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const decode = jwt.verify(token,config.JWT_SECRET);
-  
-  const user = await userModel.findById(decode.id)
+  const decode = jwt.verify(token, config.JWT_SECRET);
+
+  const user = await userModel.findById(decode.id);
 
   res.status(200).json({
-    message:"User fetched successfully",
-    user:{
-      id:user._id,
-      username:user.username,
-      email:user.email
-    }
-  })
-  
+    message: "User fetched successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+};
 
-}
+module.exports.refreshToken = async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const decode = jwt.verify(refreshToken, config.JWT_SECRET);
+  const acessToken = jwt.sign({ id: decode.id }, config.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const newRefreshToken = jwt.sign({ id: decode.id }, config.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  
+  res.cookie("refreshToken", newRefreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+  
+  res.status(200).json({
+    message: "Access token refreshed successfully",
+    accessToken: acessToken,
+  });
+
+
+  const user = await userModel.findById(decode.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,4 +5,5 @@ const controllers = require("../controllers/auth.controller");
 router.post("/register",controllers.Register)
 router.post("/login",controllers.Login)
 router.get("/getme",controllers.getme)
+router.post("/refresh-Token",controllers.refreshToken)
 module.exports = router;
